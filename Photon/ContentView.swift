c78efe997dc9e0df96e2aa8d48ab15ddca244f82
aadishiv <aadishiv@outlook.com>
2024-12-30 00:20:20 -0500

    init

---
+++ Photon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Photon
+//
+//  Created by Aadi Shiv Malhotra on 12/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Photon/PhotonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotonApp.swift
+//  Photon
+//
+//  Created by Aadi Shiv Malhotra on 12/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
